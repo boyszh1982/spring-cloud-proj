e224f78f8b9655535f82dffb6e2afb359e6b0f8f
boyszh1982@163.com <boyszh1982@163.com>
2017-02-18 11:49:21 +0800

    learning plan

--- reservation-service/src/main/java/org/test/DemoApplication.java
+++ reservation-service/src/main/java/org/test/DemoApplication.java
@@ -19,7 +19,12 @@ public class DemoApplication {
 				.forEach( x -> rr.save(new Reservation(x)) );
 			rr.findAll().forEach(System.out::println);
 		} ;
-		
+		/*	
+		 * branch master is all of spring-cloud
+		 * next step branch spring-cloud-step1-simple
+		 * next step branch spring-cloud-step2-security-config
+		 * next step branch spring-cloud-step3-eureka
+		 */
 	}
 	
 	public static void main(String[] args) {
